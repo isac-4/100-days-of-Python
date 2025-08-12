7ee906bc1d1bb0684e214a5da3fe5ae9d3acc066
isac-4 <shaikishak773@gmail.com>
2025-08-12 21:00:16 +0530

    First Project

---
+++ project-1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("Which city do you grew up in?\n")
+pet = input("what's the name of your pet?\n")
+print("your band name could be " + city + " " + pet)
